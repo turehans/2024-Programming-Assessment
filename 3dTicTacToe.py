77678ffc71c66b530c31d7d8e72d46f407471166
ture.hansson <ture.hansson@aquinas.school.nz>
2024-03-12 13:54:07 +1300

    generate starting board

--- 3dTicTacToe.py
+++ 3dTicTacToe.py
@@ -11,19 +11,35 @@ logo = """
                                                                                                          
 """
 
+BOARD_SIZE = 3
+
 
 def welcome_message():
     print(logo)
     print("Welcome to 3d Tic Tac Toe")
     print("Rules:")
-    print("1. The game is played on a 3x3x3 grid.")
+    print("1. The game is played on a 3d grid.")
     print("2. Players take turns placing their marks (X or O) on an empty cell.")
     print("3. The goal is to get three marks in a row, column, or diagonal.")
     print("4. The game ends in a draw if all cells are filled without a winner.")
 
 
+def generate_starting_board():
+    global BOARD_SIZE
+    board = {}
+    for i in range(BOARD_SIZE):
+        for j in range(BOARD_SIZE):  # Added parentheses to close the dictionary update statement
+            board.update({f"{i*BOARD_SIZE+j}": [' '] * BOARD_SIZE})
+
+    return board
+
+
 def main():
+
+    board = {}
     welcome_message()
+    board = generate_starting_board()
+    print(board)
 
 
 if __name__ == "__main__":
