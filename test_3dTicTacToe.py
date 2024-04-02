a383b829755591710f1f92d963c5dbb99ff132f2
ture.hansson <ture.hansson@aquinas.school.nz>
2024-04-03 09:47:39 +1300

    Test update board function

--- test_3dTicTacToe.py
+++ test_3dTicTacToe.py
@@ -76,3 +76,56 @@ def check_index_is_returned_correctly_get_player_input():
         print("Test 5 passed")
     except AssertionError:
         print("Test 5 failed")
+
+
+def test_update_board():
+
+    # test 1 board index is empty boundary case 1
+    board = {'0': [' ', ' ', ' '], '3': [' ', ' ', ' '], '6': [' ', ' ', ' '], '1': [' ', ' ', ' '], '4': [
+        ' ', ' ', ' '], '7': [' ', ' ', ' '], '2': [' ', ' ', ' '], '5': [' ', ' ', ' '], '8': [' ', ' ', ' ']}
+
+    test = TicTacToe.update_board(board, 'X', '0')
+
+    try:
+        assert test['0'] == ['X', ' ', ' ']
+        print("Test 1 passed")
+    except AssertionError:
+        print("Test 1 failed")
+
+    # test 2 board index is empty boundary case 2
+
+    board = {'0': [' ', ' ', ' '], '3': [' ', ' ', ' '], '6': [' ', ' ', ' '], '1': [' ', ' ', ' '], '4': [
+        ' ', ' ', ' '], '7': [' ', ' ', ' '], '2': [' ', ' ', ' '], '5': [' ', ' ', ' '], '8': [' ', ' ', ' ']}
+
+    test = TicTacToe.update_board(board, 'X', '8')
+
+    try:
+        assert test['8'] == ['X', ' ', ' ']
+        print("Test 2 passed")
+    except AssertionError:
+        print("Test 2 failed")
+
+    # test 3 board index is partially full expected case
+
+    board = {'0': ['X', 'O', ' '], '3': ['X', 'O', ' '], '6': [' ', ' ', ' '], '1': [' ', ' ', ' '], '4': [
+        ' ', ' ', ' '], '7': ['X', 'X', ' '], '2': [' ', ' ', ' '], '5': ['X', 'O', ' '], '8': [' ', ' ', ' ']}
+    test = TicTacToe.update_board(board, 'X', '0')
+    try:
+        assert test['0'] == ['X', 'O', 'X']
+        print("Test 3 passed")
+    except AssertionError:
+        print("Test 3 failed")
+
+    # test 4 board index is partially full expected case
+
+    board = {'0': ['X', 'O', ' '], '3': ['X', 'O', ' '], '6': [' ', ' ', ' '], '1': [' ', ' ', ' '], '4': [
+        ' ', ' ', ' '], '7': ['X', 'X', ' '], '2': [' ', ' ', ' '], '5': ['X', ' ', ' '], '8': [' ', ' ', ' ']}
+    test = TicTacToe.update_board(board, 'X', '5')
+    try:
+        assert test['5'] == ['X', 'X', ' ']
+        print("Test 4 passed")
+    except AssertionError:
+        print("Test 4 failed")
+
+
+test_update_board()
