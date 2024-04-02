2b6ac7abd0792363c34f9c7f4119877197088b72
ture.hansson <ture.hansson@aquinas.school.nz>
2024-04-03 12:41:05 +1300

    testing check for win function

--- test_3dTicTacToe.py
+++ test_3dTicTacToe.py
@@ -128,4 +128,342 @@ def test_update_board():
         print("Test 4 failed")
 
 
-test_update_board()
+def call_check_for_winner(board):
+    return TicTacToe.check_for_win(board)
+
+
+def test_check_for_winner():
+    check_rows()
+    check_columns()
+    check_vertical()
+    check_flat_diagonals()
+    check_3d_diagonals_x_is_constant()
+    check_3d_diagonals_y_is_constant()
+    check_3d_diagonals_nothing_is_constant()
+
+
+def check_rows():
+
+    print("Testing check_rows")
+    # Test case 1:
+    board = {'0': ['X', ' ', ' '], '3': ['X', ' ', ' '], '6': ['X', ' ', ' '], '1': ['O', ' ', ' '], '4': [
+        ' ', ' ', ' '], '7': [' ', ' ', ' '], '2': [' ', ' ', ' '], '5': [' ', ' ', ' '], '8': [' ', ' ', ' ']}
+    try:
+        assert call_check_for_winner(board) == True
+        print("Test 1 passed")
+    except AssertionError:
+        print("Test 1 failed")
+
+    # Test case 2:
+    board = {'0': [' ', ' ', ' '], '3': [' ', ' ', ' '], '6': [' ', ' ', ' '], '1': ['O', ' ', ' '], '4': [
+        'O', ' ', ' '], '7': ['O', ' ', ' '], '2': [' ', ' ', ' '], '5': [' ', ' ', ' '], '8': [' ', ' ', ' ']}
+    try:
+        assert call_check_for_winner(board) == True
+        print("Test 2 passed")
+    except AssertionError:
+        print("Test 2 failed")
+
+    # Test case 3:
+    board = {'0': [' ', ' ', ' '], '3': [' ', ' ', ' '], '6': [' ', ' ', ' '], '1': [' ', ' ', ' '], '4': [
+        ' ', ' ', ' '], '7': [' ', ' ', ' '], '2': [' ', ' ', ' '], '5': [' ', ' ', ' '], '8': [' ', ' ', ' ']}
+    try:
+        assert call_check_for_winner(board) == False
+        print("Test 3 passed")
+    except AssertionError:
+        print("Test 3 failed")
+
+    # Test case 4:
+    board = {'0': ['X', ' ', ' '], '3': ['O', ' ', ' '], '6': ['X', ' ', ' '], '1': [' ', ' ', ' '], '4': [
+        ' ', ' ', ' '], '7': [' ', ' ', ' '], '2': ['O', ' ', ' '], '5': ['X', ' ', ' '], '8': [' ', ' ', ' ']}
+    try:
+        assert call_check_for_winner(board) == False
+        print("Test 4 passed")
+    except AssertionError:
+        print("Test 4 failed")
+
+    # Test case 5:
+    board = {'0': ['X', 'O', 'X'], '3': ['X', 'O', ' '], '6': ['O', 'O', ' '], '1': ['O', '', ' '], '4': [
+        ' ', ' ', ' '], '7': ['O', ' ', ' '], '2': ['X', 'O', ' '], '5': [' ', ' ', ' '], '8': [' ', ' ', ' ']}
+    try:
+        assert call_check_for_winner(board) == True
+        print("Test 5 passed")
+    except AssertionError:
+        print("Test 5 failed")
+
+    print("\n\n")
+
+
+def check_columns():
+    print("Testing check_columns")
+    # Test case 1
+    board = {'0': ['X', ' ', ' '], '3': ['O', ' ', ' '], '6': ['X', ' ', ' '], '1': ['X', ' ', ' '], '4': [
+        ' ', ' ', ' '], '7': [' ', ' ', ' '], '2': ['X', ' ', ' '], '5': [' ', ' ', ' '], '8': [' ', ' ', ' ']}
+    try:
+        assert call_check_for_winner(board) == True
+        print("Test 1 passed")
+    except AssertionError:
+        print("Test 1 failed")
+
+    # Test case 2:
+    board = {'0': ['O', 'O', ' '], '3': [' ', ' ', ' '], '6': [' ', ' ', ' '], '1': ['O', ' ', ' '], '4': [
+        ' ', ' ', ' '], '7': ['O', ' ', ' '], '2': ['X', 'O', ' '], '5': [' ', ' ', ' '], '8': [' ', ' ', ' ']}
+    try:
+        assert call_check_for_winner(board) == True
+        print("Test 2 passed")
+    except AssertionError:
+        print("Test 2 failed")
+
+    # Test case 3:
+    board = {'0': [' ', ' ', ' '], '3': [' ', ' ', ' '], '6': [' ', ' ', ' '], '1': [' ', ' ', ' '], '4': [
+        ' ', ' ', ' '], '7': [' ', ' ', ' '], '2': [' ', ' ', ' '], '5': [' ', ' ', ' '], '8': [' ', ' ', ' ']}
+    try:
+        assert call_check_for_winner(board) == False
+        print("Test 3 passed")
+    except AssertionError:
+        print("Test 3 failed")
+
+    # Test case 4:
+    board = {'0': ['X', 'O', 'X'], '3': [' ', ' ', ' '], '6': [' ', ' ', ' '], '1': [' ', ' ', ' '], '4': [
+        ' ', ' ', ' '], '7': [' ', ' ', ' '], '2': [' ', ' ', ' '], '5': [' ', ' ', ' '], '8': [' ', ' ', ' ']}
+    try:
+        assert call_check_for_winner(board) == False
+        print("Test 4 passed")
+    except AssertionError:
+        print("Test 4 failed")
+    print("\n\n")
+
+
+def check_vertical():
+    print("Testing check_vertical")
+    # Test case 1:
+    board = {'0': ['X', 'X', 'X'], '3': [' ', ' ', ' '], '6': ['X', ' ', ' '], '1': ['O', ' ', ' '], '4': [
+        ' ', ' ', ' '], '7': [' ', ' ', ' '], '2': [' ', ' ', ' '], '5': [' ', ' ', ' '], '8': [' ', ' ', ' ']}
+    try:
+        assert call_check_for_winner(board) == True
+        print("Test 1 passed")
+    except AssertionError:
+        print("Test 1 failed")
+
+    # Test case 2:
+    board = {'0': [' ', ' ', ' '], '3': [' ', ' ', ' '], '6': [' ', ' ', ' '], '1': [' ', ' ', ' '], '4': [
+        'O', 'O', 'O'], '7': [' ', ' ', ' '], '2': [' ', ' ', ' '], '5': [' ', ' ', ' '], '8': [' ', ' ', ' ']}
+    try:
+        assert call_check_for_winner(board) == True
+        print("Test 2 passed")
+    except AssertionError:
+        print("Test 2 failed")
+
+    # Test case 3:
+    board = {'0': [' ', ' ', ' '], '3': [' ', ' ', ' '], '6': [' ', ' ', ' '], '1': [' ', ' ', ' '], '4': [
+        ' ', ' ', ' '], '7': [' ', ' ', ' '], '2': [' ', ' ', ' '], '5': [' ', ' ', ' '], '8': [' ', ' ', ' ']}
+    try:
+        assert call_check_for_winner(board) == False
+        print("Test 3 passed")
+    except AssertionError:
+        print("Test 3 failed")
+
+    # Test case 4:
+    board = {'0': ['X', ' ', ' '], '3': ['O', ' ', ' '], '6': ['X', ' ', ' '], '1': [' ', ' ', ' '], '4': [
+        ' ', ' ', ' '], '7': [' ', ' ', ' '], '2': ['O', ' ', ' '], '5': ['X', ' ', ' '], '8': [' ', ' ', ' ']}
+    try:
+        assert call_check_for_winner(board) == False
+        print("Test 4 passed")
+    except AssertionError:
+        print("Test 4 failed")
+
+    print("\n\n")
+
+
+def check_flat_diagonals():
+    print("Testing check_diagonals")
+    # Test case 1:
+    board = {'0': ['X', ' ', ' '], '3': [' ', ' ', ' '], '6': [' ', ' ', ' '], '1': ['O', ' ', ' '], '4': [
+        'X', ' ', ' '], '7': [' ', ' ', ' '], '2': [' ', ' ', ' '], '5': [' ', ' ', ' '], '8': ['X', ' ', ' ']}
+    try:
+        assert call_check_for_winner(board) == True
+        print("Test 1 passed")
+    except AssertionError:
+        print("Test 1 failed")
+
+    # Test case 2:
+    board = {'0': [' ', 'X', ' '], '3': [' ', ' ', ' '], '6': [' ', ' ', ' '], '1': [' ', ' ', ' '], '4': [
+        ' ', 'X', ' '], '7': [' ', ' ', ' '], '2': [' ', ' ', ' '], '5': [' ', ' ', ' '], '8': [' ', 'X', ' ']}
+    try:
+        assert call_check_for_winner(board) == True
+        print("Test 2 passed")
+    except AssertionError:
+        print("Test 2 failed")
+
+    # Test case 3:
+    board = {'0': [' ', ' ', ' '], '3': [' ', ' ', ' '], '6': ['X', ' ', ' '], '1': [' ', ' ', ' '], '4': [
+        'X', ' ', ' '], '7': [' ', ' ', ' '], '2': ['X', ' ', ' '], '5': [' ', ' ', ' '], '8': [' ', ' ', ' ']}
+    try:
+        assert call_check_for_winner(board) == True
+        print("Test 3 passed")
+    except AssertionError:
+        print("Test 3 failed")
+
+    # Test case 4:
+    board = {'0': [' ', ' ', ' '], '3': [' ', ' ', ' '], '6': [' ', 'X', ' '], '1': [' ', ' ', ' '], '4': [
+        ' ', 'X', ' '], '7': [' ', ' ', ' '], '2': [' ', 'X', ' '], '5': [' ', ' ', ' '], '8': [' ', ' ', ' ']}
+
+    try:
+        assert call_check_for_winner(board) == True
+        print("Test 4 passed")
+    except AssertionError:
+        print("Test 4 failed")
+
+    print("\n\n")
+
+
+def check_3d_diagonals_x_is_constant():
+    print("Testing check_3d_diagonals x is constant")
+    # Test case 1:
+    board = {'0': ['X', ' ', ' '], '3': [' ', 'X', ' '], '6': [' ', ' ', 'X'], '1': [' ', ' ', ' '], '4': [
+        ' ', ' ', ' '], '7': [' ', ' ', ' '], '2': [' ', ' ', ' '], '5': [' ', ' ', ' '], '8': [' ', ' ', ' ']}
+    try:
+        assert call_check_for_winner(board) == True
+        print("Test 1 passed")
+    except AssertionError:
+        print("Test 1 failed")
+
+    # Test case 2:
+    board = {'0': ['O', 'O', 'X'], '3': ['O', 'X', ' '], '6': ['X', ' ', ' '], '1': [' ', ' ', ' '], '4': [
+        ' ', ' ', ' '], '7': [' ', ' ', ' '], '2': [' ', ' ', ' '], '5': [' ', ' ', ' '], '8': [' ', ' ', ' ']}
+    try:
+        assert call_check_for_winner(board) == True
+        print("Test 2 passed")
+    except AssertionError:
+        print("Test 2 failed")
+
+    # Test case 3:
+    board = {'0': [' ', ' ', ' '], '3': [' ', ' ', ' '], '6': [' ', ' ', ' '], '1': ['X', ' ', ' '], '4': [
+        ' ', 'X', ' '], '7': [' ', ' ', 'X'], '2': [' ', ' ', ' '], '5': [' ', ' ', ' '], '8': [' ', ' ', ' ']}
+    try:
+        assert call_check_for_winner(board) == True
+        print("Test 3 passed")
+    except AssertionError:
+        print("Test 3 failed")
+
+    # Test case 4:
+    board = {'0': [' ', ' ', ' '], '3': [' ', ' ', ' '], '6': [' ', ' ', ' '], '1': [' ', ' ', 'X'], '4': [
+        ' ', 'X', ' '], '7': ['X', ' ', ' '], '2': [' ', ' ', ' '], '5': [' ', ' ', ' '], '8': [' ', ' ', ' ']}
+    try:
+        assert call_check_for_winner(board) == True
+        print("Test 4 passed")
+    except AssertionError:
+        print("Test 4 failed")
+
+    # Test case 5:
+    board = {'0': ['X', ' ', ' '], '3': [' ', 'O', ' '], '6': [' ', ' ', 'X'], '1': [' ', ' ', ' '], '4': [
+        ' ', ' ', ' '], '7': [' ', ' ', ' '], '2': [' ', ' ', ' '], '5': [' ', ' ', ' '], '8': [' ', ' ', ' ']}
+    try:
+        assert call_check_for_winner(board) == False
+        print("Test 5 passed")
+    except AssertionError:
+        print("Test 5 failed")
+
+    print("\n\n")
+
+
+def check_3d_diagonals_y_is_constant():
+    print("Testing check_3d_diagonals y is constant")
+    # Test case 1:
+    board = {'0': ['X', ' ', ' '], '3': [' ', ' ', ' '], '6': [' ', ' ', ' '], '1': [' ', 'X', ' '], '4': [
+        ' ', ' ', ' '], '7': [' ', ' ', ' '], '2': [' ', ' ', 'X'], '5': [' ', ' ', ' '], '8': [' ', ' ', ' ']}
+    try:
+        assert call_check_for_winner(board) == True
+        print("Test 1 passed")
+    except AssertionError:
+        print("Test 1 failed")
+
+    # Test case 2:
+    board = {'0': [' ', ' ', 'X'], '3': [' ', ' ', ' '], '6': [' ', ' ', ' '], '1': ['X', ' ', ' '], '4': [
+        ' ', ' ', ' '], '7': [' ', ' ', ' '], '2': ['X', ' ', ' '], '5': [' ', ' ', ' '], '8': [' ', ' ', ' ']}
+    try:
+        assert call_check_for_winner(board) == True
+        print("Test 2 passed")
+    except AssertionError:
+        print("Test 2 failed")
+
+    # Test case 3:
+    board = {'0': [' ', ' ', ' '], '3': ['X', ' ', ' '], '6': [' ', ' ', ' '], '1': [' ', ' ', ' '], '4': [
+        ' ', 'X', ' '], '7': [' ', ' ', ' '], '2': [' ', ' ', ' '], '5': [' ', ' ', 'X'], '8': [' ', ' ', ' ']}
+    try:
+        assert call_check_for_winner(board) == True
+        print("Test 3 passed")
+    except AssertionError:
+        print("Test 3 failed")
+
+    # Test case 4:
+    board = {'0': [' ', ' ', ' '], '3': [' ', ' ', 'X'], '6': [' ', ' ', ' '], '1': [' ', ' ', ' '], '4': [
+        ' ', 'X', ' '], '7': [' ', ' ', ' '], '2': [' ', ' ', ' '], '5': ['X', ' ', ' '], '8': [' ', ' ', ' ']}
+    try:
+        assert call_check_for_winner(board) == True
+        print("Test 4 passed")
+    except AssertionError:
+        print("Test 4 failed")
+
+    # Test case 5:
+    board = {'0': ['X', ' ', ' '], '3': [' ', 'O', ' '], '6': [' ', ' ', 'X'], '1': [' ', ' ', ' '], '4': [
+        ' ', ' ', ' '], '7': [' ', ' ', ' '], '2': [' ', ' ', ' '], '5': [' ', ' ', ' '], '8': [' ', ' ', ' ']}
+    try:
+        assert call_check_for_winner(board) == False
+        print("Test 5 passed")
+    except AssertionError:
+        print("Test 5 failed")
+
+    print("\n\n")
+
+
+def check_3d_diagonals_nothing_is_constant():
+    print("Testing check_3d_diagonals nothing is constant")
+    # Test case 1:
+    board = {'0': ['X', ' ', ' '], '3': [' ', ' ', ' '], '6': [' ', ' ', ' '], '1': [' ', ' ', ' '], '4': [
+        ' ', 'X', ' '], '7': [' ', ' ', ' '], '2': [' ', ' ', ' '], '5': [' ', ' ', ' '], '8': [' ', ' ', 'X']}
+    try:
+        assert call_check_for_winner(board) == True
+        print("Test 1 passed")
+    except AssertionError:
+        print("Test 1 failed")
+
+    # Test case 2:
+    board = {'0': ['O', 'O', 'X'], '3': [' ', ' ', ' '], '6': [' ', ' ', ' '], '1': [' ', ' ', ' '], '4': [
+        'O', 'X', ' '], '7': [' ', ' ', ' '], '2': [' ', ' ', ' '], '5': [' ', ' ', ' '], '8': ['X', ' ', ' ']}
+    try:
+        assert call_check_for_winner(board) == True
+        print("Test 2 passed")
+    except AssertionError:
+        print("Test 2 failed")
+
+    # Test case 3:
+    board = {'0': [' ', ' ', ' '], '3': [' ', ' ', ' '], '6': [' ', ' ', 'X'], '1': [' ', ' ', ' '], '4': [
+        ' ', 'X', ' '], '7': [' ', ' ', ' '], '2': ['X', ' ', ' '], '5': [' ', ' ', ' '], '8': [' ', ' ', ' ']}
+    try:
+        assert call_check_for_winner(board) == True
+        print("Test 3 passed")
+    except AssertionError:
+        print("Test 3 failed")
+
+    # Test case 4:
+    board = {'0': [' ', ' ', ' '], '3': [' ', ' ', 'X'], '6': ['X', ' ', ' '], '1': [' ', ' ', ' '], '4': [
+        ' ', 'X', ' '], '7': [' ', ' ', ' '], '2': [' ', ' ', 'X'], '5': ['X', ' ', ' '], '8': [' ', ' ', ' ']}
+    try:
+        assert call_check_for_winner(board) == True
+        print("Test 4 passed")
+    except AssertionError:
+        print("Test 4 failed")
+
+    # Test case 5:
+    board = {'0': ['X', ' ', ' '], '3': [' ', 'O', ' '], '6': [' ', ' ', 'X'], '1': [' ', ' ', ' '], '4': [
+        ' ', ' ', ' '], '7': [' ', ' ', ' '], '2': [' ', ' ', ' '], '5': [' ', ' ', ' '], '8': [' ', ' ', ' ']}
+    try:
+        assert call_check_for_winner(board) == False
+        print("Test 5 passed")
+    except AssertionError:
+        print("Test 5 failed")
+
+    print("\n\n")
+
+
+test_check_for_winner()
